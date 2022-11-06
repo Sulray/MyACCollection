d0c00e437447291301e4e1ee9acaa00df862e91b
sulray <clement_lucas@telecom-sudparis.eu>
2022-11-06 01:57:32 +0100

    added nice twig, list in easyadmin

--- src/Controller/Admin/VillageCrudController.php
+++ src/Controller/Admin/VillageCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Village;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
@@ -16,6 +19,14 @@ class VillageCrudController extends AbstractCrudController
         return Village::class;
     }
 
+    public function configureActions(Actions $actions): Actions
+    {
+
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ;
+    }
+
 
     public function configureFields(string $pageName): iterable
     {
@@ -25,7 +36,8 @@ class VillageCrudController extends AbstractCrudController
         #yield TextEditorField::new('description');
         yield AssociationField::new('cards')
             ->autocomplete()
-            ->setFormTypeOption('by_reference', false);
+            ->setFormTypeOption('by_reference', false)
+            ->setTemplatePath('admin/fields/inventaire_cards.html.twig');
     }
 
 }
